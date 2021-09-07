4d308d399716906faa96f78371613ef5a1485b18
Anila Sebastian <anilaseban5001@gmail.com>
2021-09-08 03:12:03 +0530

    sofar

--- excercises/hw3_common_elements_in_lists.py
+++ excercises/hw3_common_elements_in_lists.py
@@ -1,9 +1,11 @@
 lst=[1,7,8,9,12,45,36,23,32,74]
 lst1=[5,8,7,9,6,32,23,45,65,85]
-# find common elements
-common=[]
-for i in lst:
-    for j in lst1:
-        if i==j:
-            common.append(i)
-print(common)
+# # find common elements
+# common=[]
+# for i in lst:
+#     for j in lst1:
+#         if i==j:
+#             common.append(i)
+# print(common)
+pos=0
+pos1=0
